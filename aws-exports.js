f3e24cf0ec8b70e3a4b7c4bb3b3de0a04aad647f
jiturajb <39866745+jiturajb@users.noreply.github.com>
2020-07-25 17:47:54 -0400

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
     AWS_ACCESS_KEY_ID: 'jz2n6gydjva3vhvkykdqmgis4a', // JRB - From the console, don't know if this is correct.
-    AWS_SECRET_ACCESS_KEY: 'da2-joorrzg4urflrglimxkw37e2u4', // JRB - From the console, don't know if this is correct.
+    AWS_SECRET_ACCESS_KEY: 'da2-kvizvxplfjbmpeerduvvdco3r4', // JRB - From the console, don't know if this is correct.
     HOST: 'y7txmlhelngetfhm2oblc6y6ha.appsync-api.us-east-2.amazonaws.com',
     REGION: 'us-east-2',
     PATH: '/graphql',
