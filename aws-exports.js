66b91b15df14cb22ac3f91719c246f07eab9ae93
jiturajb <39866745+jiturajb@users.noreply.github.com>
2020-07-25 15:49:14 -0400

    Update aws-exports.js

--- aws-exports.js
+++ aws-exports.js
@@ -1,14 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config = {
-    AWS_ACCESS_KEY_ID: 'gumfjdkbc5frfnr5qz5dy4rjf4',
-    AWS_SECRET_ACCESS_KEY: 'da2-aol6xk3z3fdbpk2wm5ikwv47hu',
-    HOST: 'zafez54wbvesdai2mvcohd4aea.appsync-api.us-east-1.amazonaws.com',
-    REGION: 'us-east-1',
+    AWS_ACCESS_KEY_ID: 'jz2n6gydjva3vhvkykdqmgis4a', // JRB - From the console, don't know if this is correct.
+    AWS_SECRET_ACCESS_KEY: 'da2-joorrzg4urflrglimxkw37e2u4', // JRB - From the console, don't know if this is correct.
+    HOST: 'y7txmlhelngetfhm2oblc6y6ha.appsync-api.us-east-2.amazonaws.com',
+    REGION: 'us-east-2',
     PATH: '/graphql',
     ENDPOINT: '',
-    "authenticationType": "API_KEY"
+	"authenticationType": "API_KEY"
 };
 config.ENDPOINT = "https://" + config.HOST + config.PATH;
 exports.default = config;
-
